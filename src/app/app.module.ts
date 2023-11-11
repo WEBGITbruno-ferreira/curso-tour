3c25970775c7fc00ff4cc22d6712b96eca655807
bruno.ferreira <brunoalmeidaferreira@hotmail.com>
2023-11-11 15:30:22 -0300

    ORGANIZEI O CORE MODULE COM O MATERIALMODULE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MessagesComponent } from './messages/messages.component';
-
 import { AppRoutingModule } from './app-routing.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component'
-import { MaterialModule } from './material/material.module';
+import { MessagesComponent } from './messages/messages.component';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -27,7 +25,7 @@ import { MaterialModule } from './material/material.module';
     FlexLayoutModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
